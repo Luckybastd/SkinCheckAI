6ce1129aa4f0b337a8d648347bef218a61cdd328
Luckybastd <147938164+Luckybastd@users.noreply.github.com>
2026-02-25 15:57:49 +0700

    Update page title from 'DermSight AI' to 'DermSight'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 import pandas as pd
 import io
 
-st.set_page_config(page_title="DermSight AI", layout="wide")
+st.set_page_config(page_title="DermSight", layout="wide")
 
 st.markdown("""
     <style>
